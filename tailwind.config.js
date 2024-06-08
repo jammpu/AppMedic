fdedd687715514148edf9d08b83f894a4a79e1cb
jammpu <jeanpierrebengi@gmail.com>
2024-06-07 19:45:44 -0500

    eliminacion del archivo tailwindconfig

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/templates/*.{html,gohtml,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
