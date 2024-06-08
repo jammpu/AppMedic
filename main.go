d68a353f51877b38a5a67430b494ed042f202bec
jammpu <jeanpierrebengi@gmail.com>
2024-06-07 19:53:15 -0500

    version3

--- main.go
+++ main.go
@@ -38,18 +38,6 @@ func main() {
 }
 
 // conexion base de datos
-func conexionDB() (conexion *sql.DB) {
-	Driver := "mysql"
-	User := "root"
-	Password := "root"
-	Name := "sistemabd"
-
-	conexion, err := sql.Open(Driver, User+":"+Password+"@tcp(127.0.0.1)/"+Name)
-	if err != nil {
-		panic(err.Error())
-	}
-	return conexion
-}
 
 // plantillas
 func StartLogin(w http.ResponseWriter, r *http.Request) {
